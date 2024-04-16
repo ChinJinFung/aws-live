9fd08241fb05d877992f0f791f7eca2ef3c37884
ChinJinFung <167164870+ChinJinFung@users.noreply.github.com>
2024-04-16 13:54:35 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cxggacy20y0q.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "chinjinfung-employee"
+customregion = "us-east-1"
+
